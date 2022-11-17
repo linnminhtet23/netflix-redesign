c9b9b817b5dc97e6757c2cde74069f8b7689fb86
Linn Min Htet <linnminhtet23@gmail.com>
2022-11-18 02:43:50 +0630

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ const Home = ({
       <Header />
 
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
-        <Banner netflixOriginals={netflixOriginals} />
+        {/* <Banner netflixOriginals={netflixOriginals} /> */}
 
         <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
