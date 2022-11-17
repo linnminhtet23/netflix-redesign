cc4823f00c9cd2c6ad5381b050e394a827238863
Linn Min Htet <linnminhtet23@gmail.com>
2022-11-18 01:33:05 +0630

    fixed length error

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,7 +63,7 @@ const Home = ({
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
           {/* My List */}
-          {list?.length > 0 && <Row title="My List" movies={list} />}
+          {list.length > 0 && <Row title="My List" movies={list} />}
 
 
           <Row title="Comedies" movies={comedyMovies} />
